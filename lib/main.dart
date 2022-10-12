f03c9024031fe45ed3f6ba04afd8ae35ad9dce05
SyedMuhammadRehan <syedrehan55.rs@gmail.com>
2022-10-12 17:41:36 +0500

    perf: 2nd class work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_2022_bootcamp/1st_task.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +11,148 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const FirstTask(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            "This is Home",
+            style: TextStyle(
+              color: Colors.green,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(" Heading 1"),
+                    const Text("Heading 2"),
+                    Row(
+                      children: const [
+                        Text("Hy"),
+                        Text("By"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(" Heading 1"),
+                    const Text("Heading 2"),
+                    Row(
+                      children: const [
+                        Text("Hy"),
+                        Text("By"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(" Heading 1"),
+                    const Text("Heading 2"),
+                    Row(
+                      children: const [
+                        Text("Hy"),
+                        Text("By"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            "Home 2",
+            style: TextStyle(
+              color: Colors.yellow,
+            ),
+          ),
+          const Text(
+            "Home 3",
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+          Row(
+            children: const [
+              Text("Row Text 1"),
+              Text("2"),
+              Text("3"),
+            ],
+          ),
+          const Text("Heading 5"),
+          Container(height: 50, color: const Color(0xffFBBC05)),
+        ],
+      ),
     );
   }
 }
